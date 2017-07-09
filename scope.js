d68c0318804bd046f47c56d928ff764a252603da
matehat <mathieu@damours.org>
2017-07-09 17:10:54 -0400

    wip

--- scope.js
+++ scope.js
@@ -1,17 +1,29 @@
 const {Mixin, Collection} = require('nucleotides')
 
-function bindToScope (scope, options) {
-  this.$emit('mount', options)
-  this.constructor.$emit('mount', this, options)
-  let listenForChange = () => scope.$evalAsync()
-  scope.$on('$destroy', () => {
-    this.$emit('unmount')
-    this.constructor.$emit('unmount', this)
-    this.$off('change', listenForChange)
-  })
-  this.$on('change', listenForChange)
-}
-
 module.exports = Mixin('ScopeMixin')
-  .method('bindToScope', bindToScope)
-  .implement(Collection.$$prepareCollection, bindToScope)
+  .method('bindToScope', function (scope, options) {
+    this.$emit('mount', options)
+    this.constructor.$emit('mount', this, options)
+    let listenForChange = () => scope.$evalAsync()
+    scope.$on('$destroy', () => {
+      this.$emit('unmount')
+      this.constructor.$emit('unmount', this)
+      this.$off('change', listenForChange)
+    })
+    this.$on('change', listenForChange)
+  })
+  .implement(Collection.$$prepareCollection, function (scope, options) {
+    this.bindToScope = function () {
+      this.$emit('mount', options)
+      let listenForChange = (object) => {
+        if (this.has(object)) {
+          scope.$evalAsync()
+        }
+      }
+      scope.$on('$destroy', () => {
+        this.$emit('unmount')
+        this.$model.$off('change', listenForChange)
+      })
+      this.$model.$on('change', listenForChange)
+    }
+  })
